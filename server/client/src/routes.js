4fb5d3e15e5794cd791b03ea0ce30e6e0914af68
ProgramDig <anohin921ua@gmail.com>
2023-01-16 09:39:16 +0200

    (fix) - roles bags

--- server/client/src/routes.js
+++ server/client/src/routes.js
@@ -8,61 +8,61 @@ import MainPage from "./pages/MainPage";
 import AuthPage from "./pages/AuthPage";
 
 export const useRoutes = roles => {
-    try {
-        const role = roles.toString()
-        switch (role) {
-            case 'ADMIN':
-                return (
-                    <Routes>
-                        <Route path={'/admin'} element={<AdminPage/>} exact/>
-                        <Route path={'/department-head'} element={<DepartHeadPage/>} exact/>
-                        <Route path={'/scientific-employer'} element={<ScientificEmployerPage/>} exact/>
-                        <Route path={'/teacher'} element={<TeachPage/>} exact/>
-                        <Route path={'/main'} element={<MainPage/>} exact/>
-                        <Route path={'*'} element={<Navigate to='/main' replace/>}/>
-                    </Routes>
-                )
-            case 'DEPARTMENT_HEAD':
-                return (
-                    <Routes>
-                        <Route path={'/department-head'} element={<DepartHeadPage/>} exact/>
-                        <Route path={'/teacher'} element={<TeachPage/>} exact/>
-                        <Route path={'/main'} element={<MainPage/>} exact/>
-                        <Route path={'*'} element={<Navigate to='/main' replace/>}/>
-                    </Routes>
-                )
-            case 'SCIENTIFIC_EMPLOYER':
-                return (
-                    <Routes>
-                        <Route path={'/scientific-employer'} element={<ScientificEmployerPage/>} exact/>
-                        <Route path={'/teacher'} element={<TeachPage/>} exact/>
-                        <Route path={'/main'} element={<MainPage/>} exact/>
-                        <Route path={'*'} element={<Navigate to='/main' replace/>}/>
-                    </Routes>
-                )
-            case 'TEACHER':
-                return (
-                    <Routes>
-                        <Route path={'/teacher'} element={<TeachPage/>} exact/>
-                        <Route path={'/main'} element={<MainPage/>} exact/>
-                        <Route path={'*'} element={<Navigate to='/main' replace/>}/>
-                    </Routes>
-                )
-            case 'USER':
-                return (
-                    <Routes>
-                        <Route path={'/main'} element={<MainPage/>} exact/>
-                        <Route path={'*'} element={<Navigate to='/main' replace/>}/>
-                    </Routes>
-                )
-        }
-    } catch (e) {
-        console.log('Roles', e.message)
+    let role = ''
+    if (!!roles) {
+        role = roles.toString()
     }
-    return (
-        <Routes>
-            <Route path={'/'} element={<AuthPage/>} exact/>
-            <Route path={'*'} element={<Navigate to='/' replace/>}/>
-        </Routes>
-    )
+    switch (role) {
+        case 'ADMIN':
+            return (
+                <Routes>
+                    <Route path={'/admin'} element={<AdminPage/>} exact/>
+                    <Route path={'/department-head'} element={<DepartHeadPage/>} exact/>
+                    <Route path={'/scientific-employer'} element={<ScientificEmployerPage/>} exact/>
+                    <Route path={'/teacher'} element={<TeachPage/>} exact/>
+                    <Route path={'/main'} element={<MainPage/>} exact/>
+                    <Route path={'*'} element={<Navigate to='/main' replace/>}/>
+                </Routes>
+            )
+        case 'DEPARTMENT_HEAD':
+            return (
+                <Routes>
+                    <Route path={'/department-head'} element={<DepartHeadPage/>} exact/>
+                    <Route path={'/teacher'} element={<TeachPage/>} exact/>
+                    <Route path={'/main'} element={<MainPage/>} exact/>
+                    <Route path={'*'} element={<Navigate to='/main' replace/>}/>
+                </Routes>
+            )
+        case 'SCIENTIFIC_EMPLOYER':
+            return (
+                <Routes>
+                    <Route path={'/scientific-employer'} element={<ScientificEmployerPage/>} exact/>
+                    <Route path={'/teacher'} element={<TeachPage/>} exact/>
+                    <Route path={'/main'} element={<MainPage/>} exact/>
+                    <Route path={'*'} element={<Navigate to='/main' replace/>}/>
+                </Routes>
+            )
+        case 'TEACHER':
+            return (
+                <Routes>
+                    <Route path={'/teacher'} element={<TeachPage/>} exact/>
+                    <Route path={'/main'} element={<MainPage/>} exact/>
+                    <Route path={'*'} element={<Navigate to='/main' replace/>}/>
+                </Routes>
+            )
+        case 'USER':
+            return (
+                <Routes>
+                    <Route path={'/main'} element={<MainPage/>} exact/>
+                    <Route path={'*'} element={<Navigate to='/main' replace/>}/>
+                </Routes>
+            )
+        default:
+            return (
+                <Routes>
+                    <Route path={'/'} element={<AuthPage/>} exact/>
+                    <Route path={'*'} element={<Navigate to='/' replace/>}/>
+                </Routes>
+            )
+    }
 }
