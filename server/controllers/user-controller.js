4d0bbb7bf46641b5550dc7deed15506e6fa2157d
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2023-01-31 20:59:18 +0200

    (fix) - edit func update password

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -4,24 +4,31 @@ const User = require("../models/user-model");
 class UserController {
     async updatePassword(req, res) {
         try {
-            const {id, rePassword, oldPassword, newPassword} = req.body
+            const {id, oldPassword, rePassword, newPassword} = req.body
 
             if (rePassword !== oldPassword) {
-                return res.status(200).json({message: 'Старий та новий паролі не співпадають.'})
+                return res.status(400).json({message: 'Поля старого пароля та повтора пароля різні.'})
+            }
+            if(oldPassword === newPassword) {
+                return res.status(400).json({message: 'Старий та новий паролі одинакові.'})
             }
 
             const user = await User.findOne({_id: id})
             if (!user) {
-                return res.status(200).json({message: 'Такого користувача не існує.'})
+                return res.status(400).json({message: 'Такого користувача не існує.'})
             }
-            console.log(user)
+
             const validPassword = await bcrypt.compare(oldPassword, user.password)
             if (!validPassword) {
-                return res.status(200).json({message: 'Паролі не співпадають.'})
+                return res.status(400).json({message: 'Паролі не співпадають.'})
             }
+
             const hashPassword = await bcrypt.hash(newPassword, 3)
-            const updateResponse = await User.update({id}, {password: hashPassword})
-            return res.json(updateResponse)
+            const updateResponse = await User.updateOne({_id: id}, {password: hashPassword})
+            if (updateResponse.modifiedCount !== 1) {
+                return res.status(400).json({message: 'Пароль користувача не оновлено.'})
+            }
+            return res.json({updateResponse, message: `Пароль користувача ${id} оновлено.`})
         } catch (e) {
             console.log(e.message)
             return res.status(500).json({message: 'Помилка при заміні пароля.'})
